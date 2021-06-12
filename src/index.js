54a35e32880170b5a63be2e9b2a8262c39523ce6
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-06-13 00:38:07 +0200

    Getting a Users Physical Location

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ if (module.hot) {
 }
 
 const App = () => {
+ navigator.geolocation.getCurrentPosition(
+   (position) => {
+     console.log('Latitude is :', position.coords.latitude);
+     console.log('Longitude is :', position.coords.longitude);
+   },
+   (error) => {
+     console.error('Error Code = ' + error.code + ' - ' + error.message);
+   }
+ );
   return <div>Hi there!</div>;
 };
 

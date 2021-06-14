e224a47e8b3cc3da742fa48d4feca0b8e8798cc1
Faysal Ibn Mizan <ovitahsan@gmail.com>
2021-06-14 13:39:49 +0200

    Showing Icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisolay';
 
 if (module.hot) {
   module.hot.accept();
@@ -22,7 +23,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
